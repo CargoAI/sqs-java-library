f3474b13ec3297ebe0365c0c0f24165bbd0c47ea
Arthur Saint-Genis <arthur@saintgenis.dev>
2022-11-29 16:57:31 +0800

    migrate import path following spring boot migration

--- sqs/src/main/java/co/cargoai/sqs/internal/SqsAutoConfigurationLifecycle.java
+++ sqs/src/main/java/co/cargoai/sqs/internal/SqsAutoConfigurationLifecycle.java
@@ -1,6 +1,6 @@
 package co.cargoai.sqs.internal;
 
-import javax.annotation.PreDestroy;
+import jakarta.annotation.PreDestroy;
 import lombok.RequiredArgsConstructor;
 import org.springframework.boot.context.event.ApplicationReadyEvent;
 import org.springframework.context.ApplicationListener;
