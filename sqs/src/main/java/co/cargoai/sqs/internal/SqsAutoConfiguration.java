5602bbb8e35ab5761ee4ea8b1d1cd904efa9956d
Arthur Saint-Genis <arthur@saintgenis.dev>
2022-11-30 10:48:37 +0800

    Revert "use EnableAutoConfiguration annotation"
    This reverts commit 9e0bcb44d29b3e7fc84529ee416366cbe9ca23cf.

--- sqs/src/main/java/co/cargoai/sqs/internal/SqsAutoConfiguration.java
+++ sqs/src/main/java/co/cargoai/sqs/internal/SqsAutoConfiguration.java
@@ -7,12 +7,10 @@ import java.util.List;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
-@EnableAutoConfiguration
 class SqsAutoConfiguration {
 
   private static final Logger logger = LoggerFactory.getLogger(SqsAutoConfiguration.class);
