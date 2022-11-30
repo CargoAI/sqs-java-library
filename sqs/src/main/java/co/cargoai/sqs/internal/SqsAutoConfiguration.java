cb6b334aad477d48f19611ed7087fd0275d9cdb1
Arthur Saint-Genis <arthur@saintgenis.dev>
2022-11-30 09:57:25 +0800

    add startup log

--- sqs/src/main/java/co/cargoai/sqs/internal/SqsAutoConfiguration.java
+++ sqs/src/main/java/co/cargoai/sqs/internal/SqsAutoConfiguration.java
@@ -1,21 +1,29 @@
 package co.cargoai.sqs.internal;
 
+import co.cargoai.sqs.api.DefaultExceptionHandler;
 import co.cargoai.sqs.api.SqsMessageHandlerRegistration;
 
 import java.util.List;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 class SqsAutoConfiguration {
 
+  private static final Logger logger = LoggerFactory.getLogger(SqsAutoConfiguration.class);
+
   @Bean
   SqsMessageHandlerRegistry sqsMessageHandlerRegistry(List<SqsMessageHandlerRegistration<?>> registrations) {
+    logger.info("starting configuration sqsMessageHandlerRegistry process");
     return new SqsMessageHandlerRegistry(registrations);
   }
 
   @Bean
   SqsAutoConfigurationLifecycle sqsLifecycle(SqsMessageHandlerRegistry registry) {
+    logger.info("starting configuration sqsLifecycle process");
     return new SqsAutoConfigurationLifecycle(registry);
   }
 
